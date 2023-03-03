76bac4943425c6f7604c01a1643fe979d0e9be50
KapilKharera <kapil95881@gmail.com>
2023-03-03 19:27:53 +0530

    Added Cart dropdown UI

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,11 @@ import { Form, Container, FormControl, Navbar, NavbarBrand, Nav, Dropdown, Badge
 import DropdownToggle from 'react-bootstrap/esm/DropdownToggle';
 import DropdownMenu from 'react-bootstrap/esm/DropdownMenu';
 import { FaShoppingCart } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai"
 import { CartState } from '../context/Context';
 
 const Header = () => {
-    const {state: {cart}} = CartState();
+    const {state: {cart}, dispatch} = CartState();
     return (
         <Navbar bg='dark' variant='black' style={{ height: 80 }}>
             <Container>
@@ -33,7 +34,27 @@ const Header = () => {
                             <span style={{display:"inline-blocks", margin: "0px 6px"}}>{cart.length}</span>
                         </DropdownToggle>
                         <DropdownMenu style={{ minWidth: 370 }}>
+                            {
+                                cart.length > 0 ? (
+                                    <div>
+                                        {
+                                            cart.map(product => (
+                                                <span className="cartitem" key={product.id}>
+                                                    <img src={product.image} className="cartItemImg" alt={product.name} />
+                                                    <div className="cartItemDetail">
+                                                        <span>{product.name}</span>
+                                                        <span>₹ {product.price.split(".")[0]}</span>
+                                                    </div>
+                                                    <AiFillDelete fontSize="20px" style={{cursor: "pointer", color: "red"}} onClick={() => dispatch({type: "REMOVE_FROM_CART", payload: product})} />
+                                                </span>
+                                            ))
+                                        }
+                                    </div>
+                                ) : (
+                                    
                             <span style={{padding: 10}}>Cart is empty!</span>
+                                )
+                            }
                         </DropdownMenu>
                     </Dropdown>
                 </Nav>
